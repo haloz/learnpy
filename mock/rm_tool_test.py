a7aba26a30bef2ce7fe52fd5b33ad7197c5a939a
haloz <haloz@gmx.net>
2016-02-20 14:43:35 +0100

    extend test and implementation notice the bottom-up order of patch decorator, see http://www.voidspace.org.uk/python/mock/patch.html?highlight=stack#nesting-patch-decorators : "Note that the decorators are applied from the bottom upwards. This is the standard way that Python applies decorators. The order of the created mocks passed into your test function matches this order."

--- mock/rm_tool_test.py
+++ mock/rm_tool_test.py
@@ -5,7 +5,16 @@ import unittest
 
 class RmToolTestCase(unittest.TestCase):
 
+    @mock.patch("rm_tool.os.path")
     @mock.patch("rm_tool.os")
-    def testRmTool(self, mock_os):
+    def testRmTool(self, mock_os, mock_path):
+        mock_path.isfile.return_value = False
+        rm("any file")
+        self.assertFalse(
+            mock_os.remove.called,
+            "Failed to rm file if not present."
+        )
+
+        mock_path.isfile.return_value = True
         rm("any file")
         mock_os.remove.assert_called_with("any file")
